19d1af30eb09c7a5e2ea2fe56f552617f98ddf8e
Rami Sfari <rami2sfari@gmail.com>
2021-03-03 02:51:03 +0100

    v1.0

--- src/apps/shop_managment/views/stats_views.py
+++ src/apps/shop_managment/views/stats_views.py
@@ -1,19 +1,31 @@
 from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from ..models.order import Order
+from ..models.order import Order, OrderItem
 
 
 @api_view(['GET'])
 def get_orders_by_status(request):
-    return Response(Order.objects.values('status').annotate(count=Count('status')))
+    result = Order.objects.raw("""
+        SELECT 1 id, COUNT(*) as count, status as status 
+        FROM shop_managment_order 
+        GROUP BY status""")
+    return Response([ dict(count=order.count, status=order.status) for order in result ])
 
 
 @api_view(['GET'])
 def get_orders_by_date(request):
-    return Response(Order.objects.values('created_at__month').annotate(count=Count('created_at__month')))
+    result = Order.objects.raw("""
+        SELECT 1 id, COUNT(*) as count, extract(month from created_at) as month 
+        FROM shop_managment_order 
+        GROUP BY extract(month from created_at)""")
+    return Response([ dict(count=order.count, month=order.month) for order in result ])
 
 
 @api_view(['GET'])
 def get_income_by_month(request):
-    return Response(Order.objects.values('total_cost').annotate(count=Count('created_at__month')))
+    result = OrderItem.objects.raw("""
+        SELECT 1 id, SUM(price * quantity) as price, extract(month from created_at) as month 
+        FROM shop_managment_orderItem
+        GROUP BY extract(month from created_at)""")
+    return Response([ dict(price=order.price, month=order.month) for order in result ])
